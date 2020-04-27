14bae5b6d9ef26ecc9b9921a3340cb2a62738ff6
Zachary Taylor <ztay90@gmail.com>
2020-04-26 21:20:40 -0400

    Finished Project

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import "./components/SearchBar/SearchBarContainer.js";
+import "./components/PostsContainer/PostsPage.js";
+import SearchBar from "./components/SearchBar/SearchBarContainer.js";
+import PostsPage from "./components/PostsContainer/PostsPage.js";
 
 
 const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+    {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostsPage />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import dummyData from "../../dummy-data.js"
 
 const PostsPage = () => {
   // set up state for your data
+  const [postData] = useState(dummyData)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      
+      {postData.map(p => (
+        <Post post={p} />
+      ))}
     </div>
   );
 };
